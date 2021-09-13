46333a645bdfbf5d5ca3fc61eb24ab3cfaee0d3c
MauroCG <mauro57.mauricio@gmail.com>
2021-09-13 10:32:45 -0500

    Added the endpoint in the backend for the loan request

--- api/api.py
+++ api/api.py
@@ -1,8 +1,23 @@
-import time
-from flask import Flask
+from datetime import datetime
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-	return {'time': time.time()}
+	current_time = datetime.now().time()
+	return {'time': str(current_time)}
+
+
+@app.route('/request_loan', methods = ['POST'])
+def request_loan():
+	requested_amount = float(request.form.get('requestedAmount'))
+
+	if requested_amount > 50000:
+		loan_decision = "Declined"
+	elif requested_amount == 50000:
+		loan_decision = "Undecided"
+	else:
+		loan_decision = "Approved"
+
+	return {'loanDecision': loan_decision}
